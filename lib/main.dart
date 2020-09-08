d8cbdff2848c80e2cc2e14252491986bd8d55d45
Jean-Baptiste <jnb.thery@gmail.com>
2020-09-08 15:53:22 +0200

    add copyright

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+/*
+ * Created by THERY Jean-Baptiste on 9/8/20 3:52 PM.
+ * Copyright © 2020 THERY Jean-Baptiste. All rights reserved.
+ */
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,12 +11,16 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('I\'am Rich'),
+          title: Center(
+            child: Text('I\'am Rich'),
+          ),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
           child: Image(
-            image: AssetImage("images/diamond.png"),
+            image: AssetImage(
+              "images/diamond.png",
+            ),
           ),
         ),
       ),
